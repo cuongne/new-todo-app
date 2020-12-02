dc340eea41299606f3e6a63b19b7fe903b0d7ce3
cuongne <cuontruon007@gmail.com>
2020-12-02 17:05:25 +0700

    Update

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react'
 import TodoForm from './TodoForm';
 
 function TodoList() {
-    const [todos, setTodo] = useState('');
+    const [todos, setTodos] = useState('');
+    const addTodo = todo =>{
+        if(!todo.text || /^\s*$/.test(todo.text))
+        {
+            return;
+        }
+        const newTodos = [todo, ...todos]
+
+        setTodos(newTodos);
+        console.log(...todos);
+    }
     return (
         <div>
             <h1>What plan today</h1>
-            <TodoForm/>
+            <TodoForm onSubmit={addTodo}/>
         </div>
     )
 }
